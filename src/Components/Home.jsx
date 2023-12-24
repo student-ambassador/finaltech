b63051ee242087b78c3cd9501ec809b041929bde
student-ambassador <147129237+student-ambassador@users.noreply.github.com>
2023-12-24 11:42:36 +0000

    new img aded

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/laptop.jpg";
+import image from "../images/usinglaptop.jpg";
 
 const imageAltText = "A person using pc";
 
