0d16e90f3a2d4145f4415717905da9afbede796b
student-ambassador <147129237+student-ambassador@users.noreply.github.com>
2023-12-24 10:24:42 +0000

    about img added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,15 +10,15 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/books.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "a library full of books";
 
 /**
  * Sort description that expands on your title on the Home component.
